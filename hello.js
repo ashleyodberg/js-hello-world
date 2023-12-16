a259ef70ff7aeab48889a8aefd5b506c2b473ca4
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2023-12-16 14:13:47 -0800

    <First commit on the prework>

--- hello.js
+++ hello.js
@@ -3,17 +3,39 @@ Welcome, new coder.
 Enter your code on line 7 to print 'Hello, World!' 
 to the console (the panel on the right)
 */
+console.log('Hello, World!');
 
-console.log("Hello");
+console.log('PLAY!');
 
+console.log('This is hard');
+
+console.log("Coding Rocks")
+
+console.log('2');
+console.log('2 + 2');
+console.log(2)
+console.log(2 + 2)
+
+console.log('Here is message 1', 'Here is message 2')
+
+let message = "Here is message 1, \nHere is message 2";
+console.log(message);
+
+console.log('Quoth the Raven "Nevermore"')
+
+let challenge = "Believe in yourself, you WILL succeed";
+console.log(challenge);
+
+
+/*the code needs both parentheses to run as they 'wrap' the information within, 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
+   2. Figure out what the parentheses do. Will the code work without them? It will not work without them, they wrap and sort of contain the information within.
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")? Must have both opening and closing quotation marks, seems to be a stylistic preference rather than big difference between them though.
+   4. Remove the semi-colon, ;. 
    5. Print a number. (Bonus: Print two numbers added together).
    6. Print multiple messages one after the other.
    7. Print two messages on the same line.
